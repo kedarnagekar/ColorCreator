ed7febde19500fbb46a22743dcfd8bf163ff373c
Kedar Nagekar <kedarnagekar@gmail.com>
2024-07-20 20:14:51 +0530

    - Added xcframework file.

---
+++ ColorCreator.xcframework/ios-arm64/ColorCreator.framework/Headers/ColorCreator.h
@@ -0,0 +1,18 @@
+//
+//  ColorCreator.h
+//  ColorCreator
+//
+//  Created by Kedar Nagekar on 20/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ColorCreator.
+FOUNDATION_EXPORT double ColorCreatorVersionNumber;
+
+//! Project version string for ColorCreator.
+FOUNDATION_EXPORT const unsigned char ColorCreatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorCreator/PublicHeader.h>
+
+
